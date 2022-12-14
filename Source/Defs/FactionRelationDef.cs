71b400c0646b1b1ab47733cc040b54cb189e6ac1
Ivy@RecompiledBirds <fiveggamer@gmail.com>
2022-12-14 16:05:12 -0500

    Faction relations

---
+++ Source/Defs/FactionRelationDef.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RVCRestructured.Defs
+{
+    public class FactionRelationDef : Def
+    {
+        public FactionDef factionDef;
+
+        public FactionDef otherFaction;
+
+        public int opinion;
+    }
+}
