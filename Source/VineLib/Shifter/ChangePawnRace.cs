5b4faeea18e57e8007db993e37cd45519b954a2a
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2023-10-23 16:03:21 -0400

    allow pawns to keep existing?

--- Source/VineLib/Shifter/ChangePawnRace.cs
+++ Source/VineLib/Shifter/ChangePawnRace.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using Verse.AI;
 
 namespace RVCRestructured.Shifter
 {
@@ -14,47 +15,17 @@ namespace RVCRestructured.Shifter
         {
             ThingDef oldDef = pawn.def;
             bool isHumanLike = pawn.def.race.Humanlike;
-            pawn.pather.StopDead();
-            
-            pawn.jobs.StopAll();
             pawn.Strip();
             pawn.def = toDef;
-            pawn.pather.StopDead();
-           
             if (!isHumanLike)
             {
-                /*
-                pawn.abilities = new Pawn_AbilityTracker(pawn);
-                pawn.skills = new Pawn_SkillTracker(pawn);
-                pawn.story = new Pawn_StoryTracker(pawn);
-                pawn.guest = new Pawn_GuestTracker(pawn);
-                pawn.guilt = new Pawn_GuiltTracker(pawn);
-                pawn.workSettings = new Pawn_WorkSettings(pawn);
-                pawn.royalty = new Pawn_RoyaltyTracker(pawn);
-                pawn.ideo = new Pawn_IdeoTracker(pawn);
-                pawn.style = new Pawn_StyleTracker(pawn);
-                pawn.styleObserver = new Pawn_StyleObserverTracker(pawn);
-                pawn.surroundings = new Pawn_SurroundingsTracker(pawn);
-                pawn.genes = new Pawn_GeneTracker(pawn);
-                pawn.equipment = new Pawn_EquipmentTracker(pawn);
-                pawn.apparel = new Pawn_ApparelTracker(pawn);
-                //PawnComponentsUtility.CreateInitialComponents(pawn);
-                pawn.story = new Pawn_StoryTracker(pawn);
-                if (pawn.RaceProps.IsFlesh)
-                {
-                    pawn.relations = new Pawn_RelationsTracker(pawn);
-                    pawn.psychicEntropy = new Pawn_PsychicEntropyTracker(pawn);
-                }
-                PawnComponentsUtility.AddAndRemoveDynamicComponents(pawn, false);
-                */
                 PawnComponentsUtility.CreateInitialComponents(pawn);
-                pawn.kindDef = Utils.GetKindDef(pawn.def);
+                pawn.kindDef = PawnKindDefOf.WildMan;
                 pawn.story.hairDef = PawnStyleItemChooser.RandomHairFor(pawn);
                 pawn.story.bodyType=PawnGenerator.GetBodyTypeFor(pawn);
                 pawn.story.TryGetRandomHeadFromSet(from x in DefDatabase<HeadTypeDef>.AllDefs where x.randomChosen select x);
                 PawnBioAndNameGenerator.GiveAppropriateBioAndNameTo(pawn, oldDef.label, pawn.kindDef.defaultFactionType);
             }
-            pawn.pather.StopDead();
             pawn.AllComps.Clear();
             pawn.InitializeComps();
             RVRComp comp = pawn.TryGetComp<RVRComp>();
@@ -63,8 +34,15 @@ namespace RVCRestructured.Shifter
                 comp.CleanAndGenGraphics();
                 comp.InformGraphicsDirty();                
             }
-            pawn.Drawer.renderer.graphics.ResolveAllGraphics();
             pawn.pather.StopDead();
+            typeof(Pawn_PathFollower).GetMethod("PatherFailed", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).Invoke(pawn.pather, new object[] { });
+            pawn.Drawer.renderer.graphics.ResolveAllGraphics();
+            pawn.jobs.EndCurrentJob(JobCondition.InterruptForced);
+            pawn.pather = new Pawn_PathFollower(pawn);
+            pawn.jobs=new Pawn_JobTracker(pawn);
+            Job job = JobMaker.MakeJob(JobDefOf.Wait_Wander, pawn.Position);
+            job.expiryInterval = 10;
+            pawn.jobs.TryTakeOrderedJob(job);
         }
     }
 }
