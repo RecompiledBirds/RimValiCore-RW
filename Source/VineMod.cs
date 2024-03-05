5f121dc13bc78e03bfe3bd1f2462fcd60c0d0e48
RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2024-03-05 00:06:15 -0500

    Cleanup headoffset calculations

--- Source/VineMod.cs
+++ Source/VineMod.cs
@@ -41,7 +41,6 @@ namespace RVCRestructured
             listing_Standard.Begin(inRect);
             listing_Standard.CheckboxLabeled("Enable VGUI editor: ",ref settings.VGUIEnabled);
             listing_Standard.CheckboxLabeled("Race blending enabled: ", ref settings.RaceBlender);
-            listing_Standard.CheckboxLabeled("Debug mode: ",ref VineSettings.debugMode);
             listing_Standard.End();
             base.DoSettingsWindowContents(inRect);
         }
