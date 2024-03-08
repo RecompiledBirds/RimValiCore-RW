ee92172a7323a1f20aa5f030984fc6887385b5f4
Dani <daniel_plaster@live.de>
2024-03-08 12:53:01 +0100

    some refactoring

---
+++ Source/VineLib/Windows/ColorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RVCRestructured.Windows
+{
+    public static class ColorExtensions
+    {
+        public static Color MoveTowards(this Color current, Color target, float speed = 1f)
+        {
+            return new Color
+            (
+                Mathf.MoveTowards(current.r, target.r, speed * Time.deltaTime),
+                Mathf.MoveTowards(current.g, target.g, speed * Time.deltaTime),
+                Mathf.MoveTowards(current.b, target.b, speed * Time.deltaTime)
+            );
+        }
+    }
+}
