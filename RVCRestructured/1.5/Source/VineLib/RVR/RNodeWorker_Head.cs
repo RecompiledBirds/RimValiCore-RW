70594e2f90dd2d931dee81043337f8c78db3a57f
Dani <daniel_plaster@live.de>
2024-11-29 15:51:10 +0100

    fix heads invisible in beds

---
+++ RVCRestructured/1.5/Source/VineLib/RVR/RNodeWorker_Head.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace RVCRestructured.RVR;
+
+public class RNodeWorker_Head : RNodeWorker
+{
+    public override Vector3 OffsetFor(PawnRenderNode node, PawnDrawParms parms, out Vector3 pivot)
+    {
+        Vector3 offset = base.OffsetFor(node, parms, out pivot);
+
+        if (parms.posture.InBed())
+        {
+            offset += ((RenderableDefNodeProperties_Head)node.Props).extraOffset;
+        }
+
+        return offset;
+    }
+}
+
+public class RenderableDefNodeProperties_Head : RenderableDefNodeProperties
+{
+    public readonly Vector3 extraOffset;
+}
