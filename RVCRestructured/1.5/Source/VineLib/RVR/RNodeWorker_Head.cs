3a4de6b5016eebc5f63340d3870f9a69d45dbe31
RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2024-11-30 09:34:26 -0500

    fix portraits

--- RVCRestructured/1.5/Source/VineLib/RVR/RNodeWorker_Head.cs
+++ RVCRestructured/1.5/Source/VineLib/RVR/RNodeWorker_Head.cs
@@ -7,8 +7,8 @@ public class RNodeWorker_Head : RNodeWorker
     public override Vector3 OffsetFor(PawnRenderNode node, PawnDrawParms parms, out Vector3 pivot)
     {
         Vector3 offset = base.OffsetFor(node, parms, out pivot);
-
-        if (parms.posture.InBed())
+        if(parms.Portrait) return offset;
+        if (parms.posture.InBed()&&!parms.pawn.CurrentBed().def.building.bed_showSleeperBody)
         {
             offset += ((RenderableDefNodeProperties_Head)node.Props).extraOffset;
         }
