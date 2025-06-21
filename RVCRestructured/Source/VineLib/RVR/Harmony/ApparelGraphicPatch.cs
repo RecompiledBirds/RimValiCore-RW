fd8a7014dca619da8f88375dd3133db065107930
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2025-06-21 13:35:43 -0400

    fixed a bug with styled apparel

--- RVCRestructured/Source/VineLib/RVR/Harmony/ApparelGraphicPatch.cs
+++ RVCRestructured/Source/VineLib/RVR/Harmony/ApparelGraphicPatch.cs
@@ -1,10 +1,40 @@
-﻿using RVCRestructured.Shifter;
+﻿using RimWorld;
+using RVCRestructured.Shifter;
 using UnityEngine;
 
 namespace RVCRestructured.RVR.Harmony;
 
 public static class ApparelGraphicPatch
 {
+    public static string FindPath(string path, string defaultPath, Apparel apparel, GraphicsComp comp)
+    {
+        if (ContentFinder<Texture2D>.Get($"{defaultPath}_north", false))
+        {
+            path = defaultPath;
+        }
+        else if (ContentFinder<Texture2D>.Get($"{apparel.WornGraphicPath}_north", false))
+        {
+            path = apparel.WornGraphicPath;
+        }
+        else if ((comp == null || !comp.Props.useEmptyApparelIfNoDefault) && ContentFinder<Texture2D>.Get($"{apparel.WornGraphicPath}_{BodyTypeDefOf.Thin}_north", false))
+        {
+            path = $"{apparel.WornGraphicPath}_{BodyTypeDefOf.Thin}_north";
+        }
+        else
+        {
+            string newPath = apparel.WornGraphicPath.Replace(apparel.StyleDef.defName, "");
+            if (ContentFinder<Texture2D>.Get($"{newPath}_north", false))
+            {
+                path = apparel.WornGraphicPath;
+            }
+            else if ((comp == null || !comp.Props.useEmptyApparelIfNoDefault) && ContentFinder<Texture2D>.Get($"{newPath}_{BodyTypeDefOf.Thin}_north", false))
+            {
+                path = $"{newPath}_{BodyTypeDefOf.Thin}_north";
+            }
+        }
+
+            return path;
+    }
     public static void Postfix(ref Apparel apparel, ref BodyTypeDef bodyType, ref ApparelGraphicRecord rec)
     {
         if (apparel.def.apparel.wornGraphicPath.NullOrEmpty())
@@ -18,21 +48,10 @@ public static class ApparelGraphicPatch
         {
             typeDef = shapeshifterComp.MimickedBodyType;
         }
-
+        
         Graphic graphic;
         string altPath = $"{apparel.WornGraphicPath}_{typeDef.defName}";
-        if (ContentFinder<Texture2D>.Get($"{altPath}_north", false))
-        {
-            path = altPath;
-        }
-        else if (ContentFinder<Texture2D>.Get($"{apparel.WornGraphicPath}_north", false))
-        {
-            path = apparel.WornGraphicPath;
-        }
-        else if ((comp == null || !comp.Props.useEmptyApparelIfNoDefault) && ContentFinder<Texture2D>.Get($"{apparel.WornGraphicPath}_{BodyTypeDefOf.Thin}_north", false))
-        {
-            path = $"{apparel.WornGraphicPath}_{BodyTypeDefOf.Thin}_north";
-        }
+        path = FindPath(path, altPath, apparel, comp);
 
         Shader shader = ShaderDatabase.Cutout;
         if (apparel.def.apparel.useWornGraphicMask)
