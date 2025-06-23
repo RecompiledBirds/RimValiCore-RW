f923782346070e4a8b6383625e43464fc251f263
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2025-06-22 22:54:17 -0400

    more rendertree work

--- RVCRestructured/Source/VineLib/RVR/RNodeWorker_Head.cs
+++ RVCRestructured/Source/VineLib/RVR/RNodeWorker_Head.cs
@@ -4,37 +4,20 @@ namespace RVCRestructured.RVR;
 
 public class RNodeWorker_Head : RNodeWorker
 {
-    //public override Vector3 OffsetFor(PawnRenderNode node, PawnDrawParms parms, out Vector3 pivot)
-    //{
-    //    Vector3 offset = base.OffsetFor(node, parms, out pivot);
-    //    if(parms.Portrait) return offset;
-    //    if (parms.posture.InBed()&&!parms.pawn.CurrentBed().def.building.bed_showSleeperBody)
-    //    {
-    //        offset += ((RenderableDefNodeProperties_Head)node.Props).extraOffset;
-    //    }
+    public override Vector3 OffsetFor(PawnRenderNode node, PawnDrawParms parms, out Vector3 pivot)
+    {
+        Vector3 offset = base.OffsetFor(node, parms, out pivot);
+        if (parms.Portrait) return offset;
+        if (parms.posture.InBed() && !parms.pawn.CurrentBed().def.building.bed_showSleeperBody)
+        {
+            offset += ((RenderableDefNodeProperties_Head)node.Props).extraOffset;
+        }
 
-    //    return offset;
-    //}
-    //public override Vector3 ScaleFor(PawnRenderNode node, PawnDrawParms parms)
-    //{
-    //    if (node is not RenderableDefNode rNode) return base.ScaleFor(node, parms);
-    //    Pawn pawn = parms.pawn;
-    //    if (ModsConfig.BiotechActive)
-    //    {
-    //        LifeStageDef lifeStageDef = pawn.ageTracker.CurLifeStage;
-    //        if (lifeStageDef.bodySizeFactor is float scale && rNode.RProps.lifeStageOverrides.TryGetValue(lifeStageDef, out float extraScale))
-    //        {
-    //            return base.ScaleFor(node, parms) * scale * extraScale;
-    //        }
-    //    }
-    //    return base.ScaleFor(node, parms);
-    //}
-
+        return offset;
+    }
 }
 
 public class RenderableDefNodeProperties_Head : RenderableDefNodeProperties
 {
     public readonly Vector3 extraOffset;
-
-    
 }
