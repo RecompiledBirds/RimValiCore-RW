0531764c79faf543ecb9b632dfa2446b55137e0f
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2025-06-26 11:57:33 -0400

    remove old name gen code

--- RVCRestructured/Source/VineLib/RVR/Harmony/NamePatch.cs
+++ RVCRestructured/Source/VineLib/RVR/Harmony/NamePatch.cs
@@ -11,14 +11,13 @@ public static class NamePatch
 
     public static bool Prefix(ref Name __result, Pawn pawn, NameStyle style = NameStyle.Full, string? forcedLastName = null)
     {
-        if (pawn.TryGetComp<RVRComp>()==null) return true;
-        __result= GenName(out NameTriple name, pawn);
+        if (pawn.TryGetComp<RVRComp>() == null) return true;
+        __result = GenName(out NameTriple name, pawn);
 
 
-        if (Rand.Chance(0.01f))
+        if (Rand.Chance(0.2f) && SteamUtility.SteamPersonaName != "???")
         {
-            __result = new NameTriple(UnityEngine.Random.Range(1, 100) != 30 ? name.First : SteamUtility.SteamPersonaName, name.Nick ?? name.First, name.Last);
-            return true;
+            __result = new NameTriple(UnityEngine.Random.Range(1, 100) < 5 ? name.First : SteamUtility.SteamPersonaName, name.Nick ?? name.First, name.Last);
         }
         return false;
     }
