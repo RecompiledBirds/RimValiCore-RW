5de8878271517feda169a1fea488cee2675ed9a0
RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2025-07-15 09:53:46 -0400

    Update ApparelGraphicPatch.cs

--- RVCRestructured/Source/VineLib/RVR/Harmony/ApparelGraphicPatch.cs
+++ RVCRestructured/Source/VineLib/RVR/Harmony/ApparelGraphicPatch.cs
@@ -6,9 +6,9 @@ namespace RVCRestructured.RVR.Harmony;
 
 public static class ApparelGraphicPatch
 {
-    public static string FindPath(string path, string defaultPath,BodyTypeDef bodyTypeDef, Apparel apparel, GraphicsComp comp)
+    public static string FindPath(string defaultPath,BodyTypeDef bodyTypeDef, Apparel apparel, GraphicsComp comp)
     {
-        
+        string path ="RVC/Empty";
         if (ContentFinder<Texture2D>.Get($"{defaultPath}_north", false))
         {
             return $"{defaultPath}";
@@ -17,7 +17,8 @@ public static class ApparelGraphicPatch
         {
             return apparel.WornGraphicPath;
         }
-        if ((comp == null || !comp.Props.useEmptyApparelIfNoDefault) && ContentFinder<Texture2D>.Get($"{apparel.WornGraphicPath}_{bodyTypeDef}_north", false))
+        bool dontUseEmpty=comp == null || !comp.Props.useEmptyApparelIfNoDefault;
+        if (dontUseEmpty && ContentFinder<Texture2D>.Get($"{apparel.WornGraphicPath}_{bodyTypeDef}_north", false))
         {
             return $"{apparel.WornGraphicPath}_{bodyTypeDef}";
         }
@@ -27,7 +28,7 @@ public static class ApparelGraphicPatch
         {
             return $"{newPath}";
         }
-        else if ((comp == null || !comp.Props.useEmptyApparelIfNoDefault) && ContentFinder<Texture2D>.Get($"{newPath}_{bodyTypeDef}_north", false))
+        else if (dontUseEmpty && ContentFinder<Texture2D>.Get($"{newPath}_{bodyTypeDef}_north", false))
         {
             return $"{newPath}_{bodyTypeDef}";
         }
@@ -43,7 +44,6 @@ public static class ApparelGraphicPatch
         Graphic resultGraphic;
         Pawn pawn = apparel.Wearer;
         BodyTypeDef copyBodyType = bodyType;
-        string path = "RVC/Empty";
         GraphicsComp comp = pawn.TryGetComp<GraphicsComp>();
         ShapeshifterComp shapeshifterComp = pawn.TryGetComp<ShapeshifterComp>();
         if (comp == null)
@@ -56,7 +56,7 @@ public static class ApparelGraphicPatch
             copyBodyType = shapeshifterComp.MimickedBodyType;
         }
         string defaultPath = $"{apparel.WornGraphicPath}_{copyBodyType.defName}";
-        path = FindPath(path, defaultPath,copyBodyType, apparel, comp);
+        string path = FindPath(path, defaultPath,copyBodyType, apparel, comp);
         Shader shader = ShaderDatabase.CutoutComplex;
         resultGraphic = GraphicDatabase.Get<Graphic_Multi>(path, shader, apparel.def.graphicData.drawSize, apparel.DrawColor);
         rec = new ApparelGraphicRecord(resultGraphic, apparel);
